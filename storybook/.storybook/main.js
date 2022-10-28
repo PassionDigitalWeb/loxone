65bc319e795e0462dd8d14570967ab10c64d36e4
Sam Holtby <sam@passiondigital.co.uk>
2022-10-28 12:08:24 +0200

    storybook now working with css modules

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,12 +1,49 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "storybook-addon-gatsby",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+
+  webpackFinal: async (config) => {
+    config.module.rules.find(
+      (rule) => rule.test.toString() === "/\\.css$/"
+    ).exclude = /\.module\.css$/;
+
+    // Tell webpack what to do with CSS modules
+    config.module.rules.push({
+      test: /\.module\.scss$/,
+      include: path.resolve(__dirname, "../../website/src"),
+      use: [
+        {
+          loader: "style-loader",
+        },
+        {
+          loader: "css-loader",
+          options: {
+            importLoaders: 1,
+            modules: {
+              namedExport: true,
+            },
+          },
+        },
+        {
+          loader: "sass-loader",
+        },
+      ],
+    });
+
+    return config;
+  },
+};
--- website/src/components/button/index.js
+++ website/src/components/button/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
-import classnames from "classnames/bind"
+import cn from "classnames"
 import * as styles from "./button.module.scss"
-const cx = classnames.bind(styles) // <-- explicitly bind your styles
 
 function Button(props) {
-  return <div className={cx("button")}>test button</div>
+  console.log({ styles })
+  return <div className={cn(styles.button)}>test button</div>
 }
 
 export default Button
