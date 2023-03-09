e23b9135f71d354203eb65f7b8bf57a3aed79191
Sam Holtby <61827506+PassionWebTeam@users.noreply.github.com>
2023-03-09 13:40:16 +0100

    new close buttn

--- src/components/organisms/header/index.js
+++ src/components/organisms/header/index.js
@@ -22,8 +22,7 @@ const Cta = ({ link, text, className }) => {
       field={link}
       externalComponent={Component}
       internalComponent={Component}
-      className={className}
-    >
+      className={className}>
       <span>{text}</span>
     </PrismicLink>
   )
@@ -37,8 +36,9 @@ const burgerStyles = {
     left: "36px",
     top: "36px",
   },
-  bmCross: {
-    background: "#bdc3c7",
+  bmCrossButton: {
+    width: "36px",
+    height: "36px",
   },
   bmMenuWrap: {
     position: "fixed",
@@ -76,8 +76,7 @@ const MenuNavDropdown = ({ children, active }) => {
     <div
       className={classNames(styles.navDropdown, {
         [styles.active]: active,
-      })}
-    >
+      })}>
       <div className={styles.inner}>{children}</div>
     </div>
   )
@@ -89,13 +88,11 @@ const FeatureDropdown = ({ links }) => {
     <div
       className={classNames(styles.navItemWrap)}
       onMouseEnter={() => setIsActive(true)}
-      onMouseLeave={() => setIsActive(false)}
-    >
+      onMouseLeave={() => setIsActive(false)}>
       <Anchor
         className={styles.navItem}
         href="#"
-        onClick={e => e.preventDefault()}
-      >
+        onClick={e => e.preventDefault()}>
         <span>Features</span>
       </Anchor>
       <MenuNavDropdown active={isActive}>
@@ -104,8 +101,7 @@ const FeatureDropdown = ({ links }) => {
             <PrismicLink
               field={nav_link}
               externalComponent={LinkComponent}
-              internalComponent={LinkComponent}
-            >
+              internalComponent={LinkComponent}>
               <span>{nav_text}</span>
             </PrismicLink>
           )
@@ -148,17 +144,21 @@ export const Header = ({ children, hasHero, ...props }) => {
           isOpen={isOpen}
           right
           customBurgerIcon={false}
+          customCrossIcon={
+            <div className={styles.closeBtn}>
+              <i></i>
+              <i></i>
+            </div>
+          }
           styles={burgerStyles}
           onClose={() => setIsOpen(false)}
-          itemListClassName={styles.mobileMenuNav}
-        >
+          itemListClassName={styles.mobileMenuNav}>
           {siteData?.feature_dropdown_links && (
             <>
               <Anchor
                 className={styles.navItem}
                 href="#"
-                onClick={e => e.preventDefault()}
-              >
+                onClick={e => e.preventDefault()}>
                 <span>Features</span>
               </Anchor>
               <div className={styles.mobileMenuNavSub}>
@@ -168,8 +168,7 @@ export const Header = ({ children, hasHero, ...props }) => {
                       <PrismicLink
                         field={nav_link}
                         externalComponent={LinkComponent}
-                        internalComponent={LinkComponent}
-                      >
+                        internalComponent={LinkComponent}>
                         <span>{nav_text}</span>
                       </PrismicLink>
                     )
@@ -183,8 +182,7 @@ export const Header = ({ children, hasHero, ...props }) => {
               <PrismicLink
                 field={nav_link}
                 externalComponent={LinkComponent}
-                internalComponent={LinkComponent}
-              >
+                internalComponent={LinkComponent}>
                 <span>{nav_text}</span>
               </PrismicLink>
             )
@@ -197,8 +195,7 @@ export const Header = ({ children, hasHero, ...props }) => {
           isShort && styles.short,
           hasHero && styles.hasHero
         )}
-        {...props}
-      >
+        {...props}>
         <Container size="none" padding="xs">
           <div className={styles.inner}>
             <Link to="/" className={styles.logoWrap}>
@@ -225,8 +222,7 @@ export const Header = ({ children, hasHero, ...props }) => {
                             <PrismicLink
                               field={nav_link}
                               externalComponent={LinkComponent}
-                              internalComponent={LinkComponent}
-                            >
+                              internalComponent={LinkComponent}>
                               <span>{nav_text}</span>
                             </PrismicLink>
                           )
@@ -247,15 +243,13 @@ export const Header = ({ children, hasHero, ...props }) => {
                         variant="text"
                         color="inherit"
                         to={`tel:${siteData.phone_number}`}
-                        className={styles.phoneNumberBtn}
-                      >
+                        className={styles.phoneNumberBtn}>
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           width="17"
                           height="17"
                           viewBox="0 0 17 17"
-                          fill="none"
-                        >
+                          fill="none">
                           <path
                             d="M16.4062 1.78125L13.1562 1.03125C12.8125 0.9375 12.4375 1.125 12.2812 1.46875L10.7812 4.96875C10.6562 5.28125 10.75 5.625 11 5.84375L12.9062 7.40625C11.7812 9.78125 9.8125 11.7812 7.375 12.9375L5.8125 11.0312C5.59375 10.7812 5.25 10.6875 4.9375 10.8125L1.4375 12.3125C1.09375 12.4688 0.9375 12.8438 1 13.1875L1.75 16.4375C1.84375 16.7812 2.125 17 2.5 17C10.5 17 17 10.5312 17 2.5C17 2.15625 16.75 1.875 16.4062 1.78125Z"
                             fill="#69C350"
@@ -269,15 +263,13 @@ export const Header = ({ children, hasHero, ...props }) => {
                         variant="text"
                         color="inherit"
                         to={`mailto:${siteData.phone_number}`}
-                        className={styles.mailBtn}
-                      >
+                        className={styles.mailBtn}>
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           width="16"
                           height="13"
                           viewBox="0 0 16 13"
-                          fill="none"
-                        >
+                          fill="none">
                           <path
                             d="M15.6875 4.46875C14.9688 5.03125 14.0625 5.71875 10.875 8.03125C10.25 8.5 9.09375 9.53125 8 9.53125C6.875 9.53125 5.75 8.5 5.09375 8.03125C1.90625 5.71875 1 5.03125 0.28125 4.46875C0.15625 4.375 0 4.46875 0 4.625V11C0 11.8438 0.65625 12.5 1.5 12.5H14.5C15.3125 12.5 16 11.8438 16 11V4.625C16 4.46875 15.8125 4.375 15.6875 4.46875ZM8 8.5C8.71875 8.53125 9.75 7.59375 10.2812 7.21875C14.4375 4.21875 14.75 3.9375 15.6875 3.1875C15.875 3.0625 16 2.84375 16 2.59375V2C16 1.1875 15.3125 0.5 14.5 0.5H1.5C0.65625 0.5 0 1.1875 0 2V2.59375C0 2.84375 0.09375 3.0625 0.28125 3.1875C1.21875 3.9375 1.53125 4.21875 5.6875 7.21875C6.21875 7.59375 7.25 8.53125 8 8.5Z"
                             fill="#69C350"
@@ -296,8 +288,7 @@ export const Header = ({ children, hasHero, ...props }) => {
 
                     <div
                       className={styles.mobileMenu}
-                      onClick={() => setIsOpen(true)}
-                    >
+                      onClick={() => setIsOpen(true)}>
                       <i></i>
                       <i></i>
                       <i></i>
